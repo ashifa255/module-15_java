a737211685d41310bb61134d9871831c1beb5ba3
ashifa255 <astammannahifa0330@gmail.com>
2025-09-02 20:40:56 +0600

    Create index.js

---
+++ models/index.js
@@ -0,0 +1,13 @@
+// Placeholder for database models
+// This file can be expanded with actual database models
+
+const sampleModel = {
+  find: () => {
+    // Sample model method
+    return { message: "Sample model data" };
+  }
+};
+
+module.exports = {
+  sampleModel
+};
